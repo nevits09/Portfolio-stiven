e92c97b5356a1aba15835104e88f9ce3ac43a101
edwindev <erozog125@gmail.com>
2026-07-31 19:11:34 -0500

    Landing Portfolio

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import { Profile } from "./components/Profile/Profile"
+
+export const App = () => {
+  return(
+    <>
+      <Profile />
+    </>
+  )
+}
---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,10 @@
+import profileImg from '../../images/ProfileEd.png'
+
+export const Profile = () => {
+  return (
+    <header>
+        <h1>Edwincito</h1>
+        <img src={profileImg} alt="Foto perfil" />
+    </header>
+  )
+}
